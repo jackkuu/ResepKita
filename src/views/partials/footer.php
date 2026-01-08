86a4f2c71a87d233a8b22895a7a4b1fc883430fb
Ekan <78914951+jackkuu@users.noreply.github.com>
2026-01-08 10:12:42 +0700

    Add files via upload

---
+++ src/views/partials/footer.php
@@ -0,0 +1,19 @@
+<?php
+// This file includes the footer section of the web pages, typically containing copyright information and additional links.
+?>
+
+<footer>
+    <div class="container">
+        <p>&copy; <?php echo date("Y"); ?> Resepkita. All rights reserved.</p>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="cari.php">Search Recipes</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+</footer>
+
+<script src="public/js/main.js"></script>
+</body>
+</html>
